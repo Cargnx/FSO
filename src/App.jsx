8b99cacccaa5e8f4238e447b7ede4db17f418daa
Cargnx <117575681+Cargnx@users.noreply.github.com>
2025-10-28 10:12:11 +0100

    [WIP] Basic Components & passing props

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const Hello = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <p>
+        Hello {props.name}, your are {props.age} years old
+      </p>
+    </div>
+  );
+};
 
-function App() {
-  const [count, setCount] = useState(0)
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href="https://github.com/cargnx">cargnx</a>
+    </div>
+  );
+};
+
+const App = () => {
+  const now = new Date();
+  console.log(now);
+
+  const name = "Angelo";
+  const age = 26;
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Lemi" age={20} />
+      <Hello name={name} age={age} />
+      <Hello name="Kirill" age={20} />
+      <Footer />
+    </div>
+  );
+};
 
-export default App
+export default App;
